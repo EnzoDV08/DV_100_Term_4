c74d981dee26280e000c51f36a8400cb98902669
EnzoDV08 <231244@virtualwindow.co.za>
2023-10-22 23:18:19 +0200

    sgs

---
+++ script/home.js
@@ -0,0 +1,50 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow,i)=>{
+    
+    const itemNumber = movieLists[i].querySelectorAll("img").length;
+    let clickCounter = 0;
+    arrow.addEventListener("click",()=>{
+        const ratio = Math.floor(window.innerWidth / 270);
+        clickCounter++;
+        if(itemNumber - (8 + clickCounter) + (7 - ratio) >= 0){
+       movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value-225
+    }px)`;
+        } else{
+            movieLists[i].style.transform = "translateX(0)";
+            clickCounter = 0;
+        }
+    });
+
+  
+
+    console.log(movieLists[i].querySelectorAll("img").length);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const ball = document.querySelector(".toggle-ball");
+    const items = document.querySelectorAll(
+        ".container, .movie-list-title, .navbar-container, .sidebar, .left-menu-icon, .toggle"
+    );
+
+    ball.addEventListener("click", () => {
+        items.forEach(item => {
+            item.classList.toggle("active");
+        });
+        ball.classList.toggle("active");
+    });
+    console.log(Math.floor(window.innerWidth/270));
+});
+
+document.getElementById("profileDropdownButton").addEventListener("click", function() {
+    var dropdown = document.getElementById("profileDropdownContent");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+});
+
